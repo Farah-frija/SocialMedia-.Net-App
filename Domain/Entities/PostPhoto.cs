790eebeb96f13b04c07826808d67addc6b0608c8
ReemaZa <rimazarrouki9@gmail.com>
2025-02-02 23:06:22 +0100

    another test

---
+++ Domain/Entities/PostPhoto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Entities
+{
+    public class PostPhoto
+    {
+        public Guid Id { get; set; }
+        public Guid PostId { get; set; } // Foreign key to Post
+        public string Url { get; set; } // URL of the photo
+        public Post Post { get; set; } // Navigation property
+    }
+}
--- web.Api/Program.cs
+++ web.Api/Program.cs
@@ -1,15 +1,11 @@
-
-using Core.Application.Extentions;
-using Core.Application.Mapper.FollowMapper;
-using Core.Application.Mapper.UserMapper;
+using Core.Application.Interfaces;
+using Core.Application.Services;
+using Core.Domain.RepositoryInterfaces;
+using Infrastructure.Data;
 using Infrastructure.Extentions;
-using Infrastructure.Identity.Configurations;
-using Infrastructure.Identity.Mappings;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
-
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,44 +14,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAppDbContext(builder.Configuration);
 
 builder.Services.AddInfrastructureIdentityServices();
-builder.Services.AddAutoMapper(
-    typeof(IdentityMapping).Assembly,
-    typeof(UserProfile).Assembly,
-    typeof(FollowProfile).Assembly
-);
-var jwtSection = builder.Configuration.GetSection("JWTBearerTokenSettings");
-builder.Services.Configure<JWTBearerTokenSetting>(jwtSection); // Ajoute les paramètres JWT au conteneur d'injection de dépendances
-
-var jwtBearerTokenSettings = jwtSection.Get<JWTBearerTokenSetting>(); // Récupère les paramètres JWT sous forme d'objet
-var key = Encoding.ASCII.GetBytes(jwtBearerTokenSettings.SecretKey); // Crée une clé symétrique en bytes pour la signature du JWT
-
-// Étape 2 : Ajouter l'authentification via JWT
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Définit le schéma par défaut d'authentification comme étant celui de JWT
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Définit le schéma pour les challenges d'authentification comme étant celui de JWT
-})
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false; // Indique que l'application peut accepter des tokens même s'ils sont envoyés via HTTP (modifie à true pour la production, toujours utiliser HTTPS)
-    options.SaveToken = true; // Sauvegarde le token dans la requête après son authentification
-
-    // Paramètres de validation du token JWT
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true, // Valide l'émetteur du token
-        ValidIssuer = jwtBearerTokenSettings.Issuer, // Spécifie l'émetteur attendu du token
-        ValidateAudience = true, // Valide le destinataire du token (audience)
-        ValidAudience = jwtBearerTokenSettings.Audience, // Spécifie l'audience attendue
-        ValidateIssuerSigningKey = true, // Valide la clé de signature de l'émetteur
-        IssuerSigningKey = new SymmetricSecurityKey(key), // Utilise la clé symétrique pour valider la signature du token
-        ValidateLifetime = true, // Valide la durée de vie du token (expiration)
-        ClockSkew = TimeSpan.Zero // Spécifie la tolérance de la différence d'heure entre l'horloge du serveur et celle du client
-    };
-});
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureRepoInjections();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
+builder.Services.AddScoped<IFileStorageService>(provider =>
+    new LocalFileStorageService(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -74,5 +43,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseStaticFiles();
 
 app.Run();
